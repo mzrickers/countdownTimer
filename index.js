39040556e4f45e998155dfadc9453c45311ee556
Zach Rickers <mzrickers@gmail.com>
2022-01-22 11:32:56 +0100

    added getters and setters for time remaining

--- index.js
+++ index.js
@@ -19,9 +19,17 @@ class Timer {
   }
 
   tick = () => {
-    const timeRemaining = parseFloat(this.durationInput.value);
-    this.durationInput.value = timeRemaining - 1;
+    this.timeRemaining = this.timeRemaining - 1;
   }
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value)
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+
 }
 
 const durationInput = document.querySelector("#duration");
